275233f4ce1bab611965e911d764719603b1a676
ConnerFrancis <thxbro79@gmail.com>
2021-08-12 00:48:10 -0400

    testing default push

--- custom/cobfig/bin/git-push.sh
+++ custom/cobfig/bin/git-push.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# operate from the cobfig directory
+# operate from the tf directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$SCRIPT_DIR/.."
+cd "$SCRIPT_DIR/../../.."
 
 # backup our own custom shit and instead
 # copy the default settings into custom for
 # end-user purposes
 echo ""
 echo "moving custom settings and faking the defaults:"
-cp -r ./cfg/custom ./cfg/custom-bak
-rm -r ./cfg/custom
-cp -r ./cfg/modules/settings/defaults ./cfg/custom
+cp -r ./cfg ./cfg_PUSHBACKUP
+rm -r ./cfg
+cp -r ./custom/cobfig/cfg/modules/settings/defaults ./cfg
 
 echo ""
 echo "ignored:"
@@ -21,7 +21,7 @@ git check-ignore **/*
 echo ""
 echo "adding all changed files:"
 git add -u .
-git add ./cfg/custom/*
+git add ./cfg/*
 echo ""
 echo "making commit:"
 git commit
@@ -32,5 +32,5 @@ git push
 # undo the copying nonsense
 echo ""
 echo "unfaking defaults and putting custom settings back:"
-rm -r ./cfg/custom
-mv ./cfg/custom-bak ./cfg/custom
+rm -r ./cfg
+mv ./cfg_PUSHBACKUP ./cfg
